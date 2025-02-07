45b93023537090590d766a4d6318e7990251e3fc
Edson J Vargas <edson.jhair@yahoo.com>
2025-02-06 20:45:28 -0500

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <h1 className='text-2xl'>HOME</h1>;
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch Your Startup, <br /> Connect with Enterpreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
+    </>
+  );
 };
 
 export default Home;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 const Navbar = async () => {
   const session = await auth();
+
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
       <nav className='flex justify-between items-center'>
@@ -27,7 +28,7 @@ const Navbar = async () => {
                 <button type='submit'>Logout</button>
               </form>
 
-              <Link href={`/user/${session?.id}`}>
+              <Link href={`/user/${session?.user.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
