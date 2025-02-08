db48ef5a8154b1537b22ff70875c77ada2fef278
Edson J Vargas <edson.jhair@yahoo.com>
2025-02-07 21:59:20 -0500

    All startups, startup card and Sanity setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 type Props = {
   searchParams: Promise<{ query?: string }>;
@@ -6,6 +7,18 @@ type Props = {
 
 const Home = async ({ searchParams }: Props) => {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Edson' },
+      _id: 1,
+      description: 'This is a description',
+      image: 'https://m.media-amazon.com/images/I/41ZP9GyHkKL.jpg',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
 
   return (
     <>
@@ -18,6 +31,21 @@ const Home = async ({ searchParams }: Props) => {
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search results for ${query}` : 'All startups'}
+        </p>
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
