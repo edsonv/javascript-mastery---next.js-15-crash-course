75508c38378a807fa5852f553e2e0e358fe152ed
Edson J Vargas <edson.jhair@yahoo.com>
2025-02-08 23:01:36 -0500

    Sanity Schema

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,50 @@
+import { UserIcon } from 'lucide-react';
+import { defineType } from 'sanity';
+
+export const startup = defineType({
+  name: 'startup',
+  title: 'Startups',
+  type: 'document',
+  icon: UserIcon,
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'author',
+      type: 'reference',
+      to: { type: 'author' },
+    },
+    {
+      name: 'views',
+      type: 'number',
+    },
+    {
+      name: 'description',
+      type: 'text',
+    },
+    {
+      name: 'category',
+      type: 'string',
+      validation: (Rule) =>
+        Rule.min(1).max(20).required().error('Please enter a category'),
+    },
+    {
+      name: 'image',
+      type: 'url',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'pitch',
+      type: 'markdown',
+    },
+  ],
+});
